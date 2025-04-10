2a72c6449f37b5079f97414b029a16b0fec62ded
Federico Mattiello <federico.mattiello@gmail.com>
2025-04-10 18:02:53 +0200

    Add files via upload
    Add files developed in Roche GitHub

---
+++ main-file.R
@@ -0,0 +1,8 @@
+# run ?datadrivencv::use_datadriven_cv to see more details
+datadrivencv::use_datadriven_cv(
+  full_name = "Federico Mattiello",
+  data_location = "./data/",
+  pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
+  html_location = "nickstrayer.me/datadrivencv/",
+  source_location = "https://github.com/mafed/datadrivencv/"
+)
